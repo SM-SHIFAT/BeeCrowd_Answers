c4a75fdb06cde06bf16d4c00e1ab649b8fcacc39
SM-SHIFAT <60544576+SM-SHIFAT@users.noreply.github.com>
2022-01-25 20:55:35 +0600

    Python 3.8

---
+++ 2510 - Batmain.py
@@ -0,0 +1,10 @@
+#just read. code is only 5 line.
+
+this_is_the_only_thing_you_need = int(input())
+this_doesnot_matter_batman_captures_everyone = ""
+
+for i in range(this_is_the_only_thing_you_need):
+    this_doesnot_matter_batman_captures_everyone = input()
+    
+    #Just print Y, does'nt matter who is the villen.
+    print("Y")
